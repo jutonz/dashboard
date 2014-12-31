7416a6e91f2250d895fa1348c4480b89fe03d7b3
Justin Toniazzo <justin@Karens-iMac.local>
2014-12-30 21:30:00 -0500

    Added rudimentary display tile for notes on welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
   def index
+    @tile_notes = tile_notes
+  end
+
+  def tile_notes
+    _tile_notes = Array.new
+    number_of_notes_displayed = [Note.all.size, 3].min
+    number_of_notes_displayed.times do |x|
+      _tile_notes.push Note.all.reverse[x]
+    end
+    return _tile_notes
   end
 end
