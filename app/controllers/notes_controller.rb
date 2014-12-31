d4bc293732c08da1dee895390c3e4c871f9abb41
Justin Toniazzo <jutonz42@gmail.com>
2014-12-30 22:48:19 -0500

    Improved styling for note tile.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,22 +5,24 @@ class NotesController < ApplicationController
 
   def new
     @note = Note.new
+    session[:return_to] ||= request.referer
   end
 
   def create
     @note = Note.new note_params
     @note.save
-    redirect_to notes_path
+    redirect_to session.delete(:return_to)
   end
 
   def edit
     @note = Note.find params[:id]
+    session[:return_to] ||= request.referer
   end
 
   def update
     @note = Note.find params[:id]
     @note.update! note_params
-    redirect_to notes_path
+    redirect_to session.delete(:return_to)
   end 
 
   def destroy
